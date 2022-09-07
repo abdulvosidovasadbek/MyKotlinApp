2f401d58ee92bbfff524692c41d8464ee40e2645
abdulvosidovasadbek <abdulvosidovasadbek@gmail.com>
2022-09-08 00:41:49 +0500

    Add Kotlin Project Update Update

--- app/src/main/java/org/progamer/mykotlinapp/MainActivity.kt
+++ app/src/main/java/org/progamer/mykotlinapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package org.progamer.mykotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews() {
+
+        val open_button = findViewById<Button>(R.id.open_button)
+
+        open_button.setOnClickListener {
+            callSecondActivity()
+        }
+
+    }
+
+    private fun callSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
